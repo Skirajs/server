128a8a3607682de897840e43a2666579592c1cdb
Mitchell de Rijcke <mitchell@de.rijcke.org>
2016-02-17 16:12:34 +0100

    Version 0.2.0: new approach, streamlining with Skira itself

--- bin/skira-server.js
+++ bin/skira-server.js
@@ -1,15 +1,28 @@
 #!/usr/bin/env node
-var pathTool = require("path");
-var Server = require("../lib/server");
+var fs = require("fs");
+var Server = require("../");
+
+var port = process.env.PORT;
 
 if (process.argv.length > 2) {
-	process.chdir(process.argv[2]);
+	try {
+		port = parseInt(process.argv[2]);
+	} catch (err) {
+		console.error("Invalid port specified:", process.argv[2]);
+		process.exit(1);
+	}
 }
 
-var site = require(pathTool.resolve("site"));
+try {
+	var site = require(pathTool.resolve("site"));
+} catch (err) {
+	console.error("Could not load site.js:");
+	console.error(err.stack);
+	process.exit(2);
+}
 
 var server = new Server(site);
 
-server.start(function(addr) {
+server.start(null, function(addr) {
 	console.log("Listening on %s:%d", addr.address, addr.port);
 });
